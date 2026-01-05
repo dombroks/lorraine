b8e93a2a0e7222c5d2c5c244174a6d5685936733
Raphael TEYSSANDIER <raphael.teyssandier@gmail.com>
2026-01-05 13:01:40 +0100

    fix: Gradle

--- lorraine/build.gradle.kts
+++ lorraine/build.gradle.kts
@@ -97,11 +97,12 @@ room {
 }
 
 dependencies {
-    kspCommonMainMetadata(libs.androidx.room.compiler)
-    add("kspAndroid", libs.androidx.room.compiler)
-    add("kspIosX64", libs.androidx.room.compiler)
-    add("kspIosArm64", libs.androidx.room.compiler)
-    add("kspIosSimulatorArm64", libs.androidx.room.compiler)
+    ksp(libs.androidx.room.compiler)
+//    kspCommonMainMetadata(libs.androidx.room.compiler)
+//    add("kspAndroid", libs.androidx.room.compiler)
+//    add("kspIosX64", libs.androidx.room.compiler)
+//    add("kspIosArm64", libs.androidx.room.compiler)
+//    add("kspIosSimulatorArm64", libs.androidx.room.compiler)
 }
 
 tasks.withType<KotlinNativeCompile>().configureEach {
